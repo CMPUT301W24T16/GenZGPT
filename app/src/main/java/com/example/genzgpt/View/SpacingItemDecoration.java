0089bfe05c6621b0be750291b9d3088e2e9d96a6
sqiggles <97560927+sqiggles@users.noreply.github.com>
2024-03-07 15:59:16 -0700

    implemented user browse and user delete.

---
+++ app/src/main/java/com/example/genzgpt/View/SpacingItemDecoration.java
@@ -0,0 +1,18 @@
+package com.example.genzgpt.View;
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SpacingItemDecoration extends RecyclerView.ItemDecoration {
+    private int spacing;
+
+    public SpacingItemDecoration(int spacing) {
+        this.spacing = spacing;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.bottom = spacing;
+    }
+}
