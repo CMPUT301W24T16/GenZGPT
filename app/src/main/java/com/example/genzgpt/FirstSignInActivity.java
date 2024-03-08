48cf7ebabdb4d58935c3ace5b2f4d96a7a58a0b6
tsteele1 <tylersteele557@gmail.com>
2024-03-08 10:54:05 -0700

    Trying to get First sign in to work.

---
+++ app/src/main/java/com/example/genzgpt/FirstSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.genzgpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_sign_in);
+    }
+}
