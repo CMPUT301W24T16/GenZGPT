a1de68a989f3a43411359da934ad4df2a8379e73
Faique Ahmad <faique@ualberta.ca>
2024-03-08 11:33:05 -0700

    Resolve conflicts

--- app/src/main/java/com/example/genzgpt/View/EventListFragment.java
+++ app/src/main/java/com/example/genzgpt/View/EventListFragment.java
@@ -1,17 +1,18 @@
 package com.example.genzgpt.View;
 
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
