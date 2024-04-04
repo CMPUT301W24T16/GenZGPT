31f3597e8f3f617dcc5f98c6991ba174eda9a7c1
FaiqueAhmaduAlberta <faique@ualberta.ca>
2024-04-04 17:45:16 -0600

    Resolved some warnings

--- app/src/main/java/com/example/genzgpt/View/CheckInQRCodeFragment.java
+++ app/src/main/java/com/example/genzgpt/View/CheckInQRCodeFragment.java
@@ -12,8 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.example.genzgpt.Model.Event;
 import com.example.genzgpt.Model.QRCodeGenerator;
 import com.example.genzgpt.R;
@@ -39,13 +39,10 @@ public class CheckInQRCodeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_check_in_qr_code, container, false);
         qrCodeImageView = view.findViewById(R.id.qr_code_image_view);
         ImageView backArrowImageView = view.findViewById(R.id.backArrowImageView);
-        backArrowImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Perform action on back arrow click, typically go back
-                if (getActivity() != null) {
-                    getActivity().onBackPressed();
-                }
+        backArrowImageView.setOnClickListener(v -> {
+            // Perform action on back arrow click, typically go back
+            if (getActivity() != null) {
+                getActivity().onBackPressed();
             }
         });
         displayCheckInQrCode();
