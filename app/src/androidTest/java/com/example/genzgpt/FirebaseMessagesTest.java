e82a7887fef9927171961a308e808e71a806e4bf
tsteele1 <tylersteele557@gmail.com>
2024-04-08 15:27:19 -0600

    XML and a few more tests.

--- app/src/androidTest/java/com/example/genzgpt/FirebaseMessagesTest.java
+++ app/src/androidTest/java/com/example/genzgpt/FirebaseMessagesTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
@@ -25,15 +26,18 @@ public class FirebaseMessagesTest {
     FirebaseMessages messages = new FirebaseMessages(getApplicationContext());
     Firebase firebase = new Firebase();
     User messageTestUser = new User("Messages", "Test", 1234567890,
-            "MessageTesting@deleteThis.now", false, null);
+            "Ihopethisworks@mail.cosmos", false, null);
+    String token;
 
     @Test
     public void testFMS() {
+        // create user.
+        // call getDeviceToken(), pass in the user id, call getStoredDeviceToken() send message to returned device token
         firebase.createUser(messageTestUser, new Firebase.OnUserCreatedListener() {
             @Override
             public void onUserCreated(String userId) {
-                Log.d("FMSTEST", userId);
-                messages.FMSFlow(userId);
+                Log.d("FMSTEST", "ID Created");
+                messageTestUser.setId(userId);
             }
 
             @Override
@@ -54,13 +58,21 @@ public class FirebaseMessagesTest {
             Log.e("FMSTEST", "TIMER ERROR IN FIRST TEST", ie);
         }
 
+        messages.getDeviceToken(messageTestUser.getId());
         String token = messages.getStoredDeviceToken();
-        assertNotNull(token);
-    }
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("MyPrefs",
+                Context.MODE_PRIVATE);
+        if (token == null) {
+            token = preferences.getString("devicetoken", null);
+        }
 
-    @Test
-    public void testNotificationSend() {
-        String token = messages.getStoredDeviceToken();
+        try{
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException ie) {
+            Log.d("FMSTEST", "WE DIDN'T GO TO THE END");
+        }
+
         assertNotNull(token);
         messages.sendMessageToDevice(token, "FMSTEST", "DID YOU GET THE FMS TEST?",
                 "message");
@@ -69,7 +81,7 @@ public class FirebaseMessagesTest {
                 getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
         try{
-            Thread.sleep(100);
+            Thread.sleep(10000);
         }
         catch (InterruptedException ie) {
             Log.d("FMSTEST", "WE DIDN'T GO TO THE END");
